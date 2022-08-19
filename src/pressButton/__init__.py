2c073bff49b5fb37797163f12cd68c2e99d4b833
Roberto Lucchetti <lucchettiroberto95@outlook.com>
2022-08-19 11:12:30 +0200

    fixing indentation

--- src/pressButton/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import like_button
--- src/pressButton/like_button.py
+++ src/pressButton/like_button.py
@@ -1,41 +1,48 @@
 import enum
 
+
 class LikeState(enum.Enum):
     empty = enum.auto()
     liked = enum.auto()
     disliked = enum.auto()
-    
+
+
 press_like_transitions = {
         LikeState.empty: LikeState.liked,
         LikeState.liked: LikeState.empty,
         LikeState.disliked: LikeState.liked,
     }
-    
+
 press_dislike_transition = {
         LikeState.empty: LikeState.disliked,
         LikeState.liked: LikeState.disliked,
         LikeState.disliked: LikeState.empty,
     }
-    
-def press_like(s:LikeState) -> LikeState:
+
+
+def press_like(s: LikeState) -> LikeState:
     return press_like_transitions[s]
 
-def press_dislike(s:LikeState) -> LikeState:
+
+def press_dislike(s: LikeState) -> LikeState:
     return press_dislike_transition[s]
 
-def press_many(s:LikeState, press: str) -> LikeState:
+
+def press_many(s: LikeState, press: str) -> LikeState:
     for c in press:
         c = c.lower()
         if c == "l":
             s = press_like(s)
         elif c == 'd':
             s = press_dislike(s)
-        else: 
+        else:
             raise ValueError("invalid press")
     return s
 
+
 def main():
     pass
-    
+
+
 if __name__ == '__main__':
-    main()         
+    main()
